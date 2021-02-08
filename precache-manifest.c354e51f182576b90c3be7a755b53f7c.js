eafc13df66aab3622e2b5fe799e99da47e3c9c25
Symphony <55611595+lnance6@users.noreply.github.com>
2021-02-07 21:56:07 -0800

    Updates

---
+++ precache-manifest.c354e51f182576b90c3be7a755b53f7c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196922907475a82c940a32a0e214fca8",
+    "url": "/mslnance-portfolio/index.html"
+  },
+  {
+    "revision": "5a4a68779d61d3c68375",
+    "url": "/mslnance-portfolio/static/css/main.883912da.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/mslnance-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mslnance-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4a68779d61d3c68375",
+    "url": "/mslnance-portfolio/static/js/main.7338b42a.chunk.js"
+  },
+  {
+    "revision": "2bf597c3d2724425835a",
+    "url": "/mslnance-portfolio/static/js/runtime-main.6cb87be0.js"
+  },
+  {
+    "revision": "37cbb62715b300d64f6276367b70bbc6",
+    "url": "/mslnance-portfolio/static/media/actively-distanced.37cbb627.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/mslnance-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "4493a8a7751bf7922ff161a71685eaba",
+    "url": "/mslnance-portfolio/static/media/covid-ca.4493a8a7.jpg"
+  },
+  {
+    "revision": "c601a76792528f18da0bccf80b468c00",
+    "url": "/mslnance-portfolio/static/media/run-buddy.c601a767.jpg"
+  },
+  {
+    "revision": "cd70b49fce937fc65b8e5e7a4064d8fc",
+    "url": "/mslnance-portfolio/static/media/taskmaster-pro.cd70b49f.jpg"
+  },
+  {
+    "revision": "8ded0b92cf46f1f3d70599cfe959a14c",
+    "url": "/mslnance-portfolio/static/media/wanderpro.8ded0b92.jpg"
+  }
+]);
